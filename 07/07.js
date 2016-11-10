2e291b43c952aa4b79d8a30e5c6c1c11b995bcfb
fumihirokinoshita <f.mihiroxx@gmail.com>
2016-11-10 10:56:26 +0900

    ex07answer

--- 07/07.js
+++ 07/07.js
@@ -1,5 +1,16 @@
+function hash(a,b){
+    return {x:a,y:b};
+}
+
+let p1 = hash()
+let p2 = hash()
+
 function distance(p1, p2){
     if(p1==p2){
         return 0;
+    }else{
+        x = p1.x - p2.x;
+        y = p1.y - p2.y;
+        return Math.abs(x) + Math.abs(y);
     }
 }
