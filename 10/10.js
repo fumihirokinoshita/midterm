79eae091652ef28a201de9390881e09e7296bbc4
fumihirokinoshita <f.mihiroxx@gmail.com>
2016-11-10 11:28:04 +0900

    ex10 add

--- 10/10.js
+++ 10/10.js
@@ -1,9 +1,19 @@
 function calc(arr){
-    if(arr[1]=="+"){
+    if(arr[1]=="+" && arr[3]==null){
         return arr[0] + arr[2];
-    }else if(arr[1]=="-"){
+    }else if(arr[1]=="-" && arr[3]==null){
         return arr[0] - arr[2];
     }else{
-        
+        let n = 0;
+        let a = arr[0];
+        while(arr[n]!=null){
+            if(arr[n+1]=="+"){
+                a = a + arr[n+2];
+            }else{
+                a = a - arr[n+2];
+            }
+        n = n + 2;
+        }
+        return a;
     }
 }
